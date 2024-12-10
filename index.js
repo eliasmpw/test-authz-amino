4e364fe814af90a4bcbde9ef05b52a6427db7132
eliasmpw <elias.poroma@philabs.xyz>
2024-12-10 14:39:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { authzSend } from "./src/authzSend.js";
 import { authzStake } from "./src/authzStake.js";
 
 // Variables
-const mnemonics = "any..."; // Replace with yours
+const mnemonics = "any..."; // Replace with yours, make sure it is a burner or testing wallet
 const rpcEndpoint = "https://rpc.constantine.archway.io";
 const granteeAddress =
   "archway1p780esyu7j4tcvc62c0ra2y042nc4mcalfetyh250h2tpz4x67wqaul9h9"; // Replace with any address, this default is the autocompounding contract on constantine
